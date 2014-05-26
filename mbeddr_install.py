c9ea6a6de0fa13e7d7e3a447cb2e88da27598b91
Zaur Molotnikov <zaur@zaurmolotnikov.com>
2014-05-26 10:51:25 +0200

    Installing from the fortiss stable

--- mbeddr_install.py
+++ mbeddr_install.py
@@ -27,7 +27,7 @@ if sys.version_info < (2, 7):
 
 # MBEDDR CONFIGURATION
 MbeddrRepo = """https://github.com/mbeddr/mbeddr.core.git"""
-#TheBranch = "fortiss_stable"
+TheBranch = "fortiss_stable"
 #TheBranch = "master"
 BuildProperties = """# MPS installation
 mps.home=MPSDir
