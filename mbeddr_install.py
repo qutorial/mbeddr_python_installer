1b538290e19b342419f490fdf884724a8607b70f
Zaur Molotnikov <zaur@zaurmolotnikov.com>
2014-06-13 15:39:52 +0200

    RC3 update

--- mbeddr_install.py
+++ mbeddr_install.py
@@ -42,10 +42,10 @@ def getMbeddrDestDir(dest):
 # MPS CONFIGURATION
 
 
-MPSMac = """http://download.jetbrains.com/mps/31/MPS-3.1-RC2-135.917-macos.dmg"""
-MPSWin = """http://download.jetbrains.com/mps/31/MPS-3.1-RC2-135.917.exe"""
-MPSLin = """http://download.jetbrains.com/mps/31/MPS-3.1-RC2-135.917.tar.gz"""
-MPSZip = """http://download.jetbrains.com/mps/31/MPS-3.1-RC2-135.917.zip"""
+MPSMac = """http://download.jetbrains.com/mps/31/MPS-3.1-RC3-135.944-macos.dmg"""
+MPSWin = """http://download.jetbrains.com/mps/31/MPS-3.1-RC3-135.944.exe"""
+MPSLin = """http://download.jetbrains.com/mps/31/MPS-3.1-RC3-135.944.tar.gz"""
+MPSZip = """http://download.jetbrains.com/mps/31/MPS-3.1-RC3-135.944.zip"""
 MPSArcDir="MPS" #This is just a part of it
 MPSVolumesDir = """/Volumes/""" #MPSVolumesDir = """/Volumes/MPS 3.1/MPS 3.1.app"""
 MPSDestDirLinux = "MPS31"
